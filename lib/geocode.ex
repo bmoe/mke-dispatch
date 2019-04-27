f91a531ea53c50369943b1bb16979c8b10e72fc1
Brian Moe <bmoe@bweeza.org>
2019-04-27 16:05:00 -0500

    add Geocode module

---
+++ lib/geocode.ex
@@ -0,0 +1,46 @@
+defmodule Geocode do
+
+  import Ecto.Query
+
+  def lookup(address) do
+    case lookup_from_database(address) do
+      {:ok, coordinates} ->
+        coordinates
+      {:error, _} ->
+        {:ok, coordinates} = lookup_from_google(address)
+        coordinates
+    end
+  end
+
+  defp lookup_from_database(address) do
+    query = from(call in Mpd.Calls.Call,
+      where: call.location == ^address and not is_nil(call.point),
+      limit: 1
+    )
+    case Mpd.Repo.one(query) do
+      nil -> {:error, nil}
+      call -> {:ok, call.point.coordinates}
+    end
+  end
+
+  defp lookup_from_google(address) do
+    IO.puts "Querying google for #{address}"
+    %{ "lat" => lat, "lng" => lng} = HTTPoison.get!(url(address))
+    |> Map.get(:body)
+    |> Poison.decode!()
+    |> Dict.get("results")
+    |> case do
+      [] -> [%{"geometry" => %{"location" => %{"lat" => nil, "lng" => nil}}}]
+      res -> res
+    end
+    |> Enum.at(0)
+    |> Dict.get("geometry")
+    |> Dict.get("location")
+    {:ok, {lat, lng}}
+  end
+
+  defp url(address) do
+    "https://maps.googleapis.com/maps/api/geocode/json?address=#{URI.encode(address)}&key=#{Application.get_env(:mpd, Geocode)[:google_api_key]}"
+  end
+
+end
