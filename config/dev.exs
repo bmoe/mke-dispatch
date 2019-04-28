120fa06fdf9948633becdd987034dfe8350b2b58
Brian Moe <bmoe@bweeza.org>
2019-04-27 22:48:44 -0500

    config Google API KEY for geocoding

--- config/dev.exs
+++ config/dev.exs
@@ -67,6 +67,9 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
+config :mpd, Geocode,
+  google_api_key: "GOOGLE_API_KEY"
+
 # Configure your database
 """
 config :mpd, Mpd.Repo,
